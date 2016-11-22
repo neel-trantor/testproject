e9654fd75364b697e8324a9ea6cd0d9028ec1dd0
Neel Karn <neelmani.karn@trantorinc.com>
2016-11-22 17:20:08 +0530

    Tabbed Activity included

--- app/src/main/java/com/londonappdeveloper/testproject/MainActivity.java
+++ app/src/main/java/com/londonappdeveloper/testproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.londonappdeveloper.testproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         startActivity(new Intent(MainActivity.this, TabActivity.class));
         // hello test comment
