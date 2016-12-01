ef2808748ac2ca9bc7bc0757b2cc1394135ea454
Neelmani.Karn <neelmani.karn@trantorinc.com>
2016-12-01 18:55:52 +0530

    hello

--- app/src/main/java/com/londonappdeveloper/testproject/Constants.java
+++ app/src/main/java/com/londonappdeveloper/testproject/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
     public static final String FACEBOOK_APP_ID = "1179370925445081";
     public static final String FACEBOOK_APP_SECRET_KEY = "1c9355ad1e9b9173f660cbe20ab682fb";
     public static final String FACEBOOK_HASHKEY = "waJc6pvspqFPhtCCTseVukhhF5s=";
+    public static final String FACEBOOK_REDIRECT_URL = "https://fir-notifications-179f6.firebaseapp.com/__/auth/handler";
 }
--- app/src/main/java/com/londonappdeveloper/testproject/MainActivity.java
+++ app/src/main/java/com/londonappdeveloper/testproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Base64;
@@ -13,14 +14,23 @@ import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.firebase.crash.FirebaseCrash;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -37,6 +47,55 @@ public class MainActivity extends AppCompatActivity {
     String mNameValue = "", mEmailValue = "";
 
     private EditText editTextEmail, editTextName;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+
+    private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            //AccessToken accessToken = loginResult.getAccessToken().getToken();
+            Profile profile = Profile.getCurrentProfile();
+            displayMessage(profile, loginResult.getAccessToken().getToken());
+            handleFacebookAccessToken(loginResult.getAccessToken().getToken());
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
+
+
+    private void handleFacebookAccessToken(String token) {
+        Log.d("TAG", "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("TAG", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("signInWithCredential", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,54 +104,48 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        mAuth = FirebaseAuth.getInstance();
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
-
-        /*LoginManager.getInstance().registerCallback(callbackManager,
-                new FacebookCallback<LoginResult>() {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // App code
-                        Toast.makeText(MainActivity.this, "sucess"+loginResult.getAccessToken(), Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        // App code
-                        Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_LONG).show();
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                        // App code
-                        Toast.makeText(MainActivity.this, "error"+exception.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-                });*/
-
-        loginButton.setReadPermissions(Arrays.asList("public_profile","user_friends"));
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        accessTokenTracker = new AccessTokenTracker() {
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                Toast.makeText(MainActivity.this, "sucess"+loginResult.getAccessToken(), Toast.LENGTH_LONG).show();
-            }
+            protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
 
-            @Override
-            public void onCancel() {
-                Toast.makeText(MainActivity.this, "cancel", Toast.LENGTH_LONG).show();
             }
+        };
 
+        profileTracker = new ProfileTracker() {
             @Override
-            public void onError(FacebookException error) {
-                Toast.makeText(MainActivity.this, "error"+error.getMessage(), Toast.LENGTH_LONG).show();
+            protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                //displayMessage(newProfile);
             }
-        });
+        };
+
+        accessTokenTracker.startTracking();
+        profileTracker.startTracking();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        //LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+
+        //LoginManager.getInstance().registerCallback(callbackManager,callback);
+
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "user_friends"));
+        loginButton.registerCallback(callbackManager, callback);
 
         //FirebaseCrash.log("Activity created");
         editTextName = (EditText) findViewById(R.id.editTextName);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
-
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
 
         try {
             PackageInfo info = getPackageManager().getPackageInfo("com.londonappdeveloper.testproject", PackageManager.GET_SIGNATURES);
@@ -109,12 +162,34 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void displayMessage(Profile profile, String accessToken) {
+        if (profile != null) {
+            Toast.makeText(MainActivity.this, "Profile" + profile.getName(), Toast.LENGTH_LONG).show();
+            Log.e("profile", profile.getProfilePictureUri(100, 100).toString());
+            Log.e("AccessToken", accessToken);
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
 
